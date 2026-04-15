d86bc4192df9ec3731cc806d8c173f33c27ffc91
empireking001 <oluwagbogoblessing@gmail.com>
2026-04-15 12:34:52 +0100

    cors 3 goodback

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ require('dotenv').config();
 const app = express();
 
 // ── Middleware ──────────────────────────────────────────────────────────────
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://licem-frontend.vercel.app/"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
